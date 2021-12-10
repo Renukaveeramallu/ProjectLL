3a701f02c84b1a13272a28b6aa38a60eb878a0ae
renukaveeramallu <renuka.veeramallu@votarytech.com>
2021-12-10 17:01:00 +0530

    program to display the LL nodes is added

---
+++ display.c
@@ -0,0 +1,17 @@
+void display()  
+{  
+    struct node *ptr;  
+    ptr = head;   
+    if(ptr == NULL)  
+    {  
+        printf("Nothing to print");  
+    }  
+    else  
+    {  
+        printf("\nprinting values . . . . .\n");   
+        while (ptr!=NULL)  
+        {  
+            printf("\n%d",ptr->data);  
+            ptr = ptr -> next;  
+        }  
+    }  
