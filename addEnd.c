446883835a1c6e6b3fae506adeca028a695a395e
renukaveeramallu <renuka.veeramallu@votarytech.com>
2021-12-10 17:10:54 +0530

    program to add nodes at the end of SLL is added

---
+++ addEnd.c
@@ -0,0 +1,34 @@
+void lastinsert()  
+{  
+    struct node *ptr,*temp;  
+    int item;     
+    ptr = (struct node*)malloc(sizeof(struct node));      
+    if(ptr == NULL)  
+    {  
+        printf("\nOVERFLOW");     
+    }  
+    else  
+    {  
+        printf("\nEnter value?\n");  
+        scanf("%d",&item);  
+        ptr->data = item;  
+        if(head == NULL)  
+        {  
+            ptr -> next = NULL;  
+            head = ptr;  
+            printf("\nNode inserted");  
+        }  
+        else  
+        {  
+            temp = head;  
+            while (temp -> next != NULL)  
+            {  
+                temp = temp -> next;  
+            }  
+            temp->next = ptr;  
+            ptr->next = NULL;  
+            printf("\nNode inserted");  
+          
+        } 
+    } 
+}
